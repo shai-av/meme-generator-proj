9c3903e9496d6fbda74ac6d46d5a5cd21d48808e
Shai <shay4447@gmail.com>
2022-06-15 11:58:12 +0300

    gallery render images

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -1,1 +1,17 @@
 'use strict'
+function onInit(){
+    renderImages()
+}
+function renderImages(){
+    let imageSrcs = getImagesSrcs()
+
+    let strHTML = ''
+    imageSrcs.forEach(src=> strHTML += `<div class="grid-item"><img src="img/${src}" onClick="onImgClk(this)"></div>`)
+
+    let gallery = document.querySelector('.gallery')
+    gallery.innerHTML = strHTML
+}
+
+function onImgClk(img){
+    console.log(img);
+}
